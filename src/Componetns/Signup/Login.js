a12f8b4d6946ec99d455565b0efaa62d34726e9c
neilchetna <neelchetna18@gmail.com>
2021-11-16 15:05:55 +0530

    first commit

---
+++ src/Componetns/Signup/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <>
+      <div id="tsparticles"></div>
+      <form>
+        <h3>Login Here</h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username" />
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password" />
+
+        <button>Log In</button>
+        <div class="social">
+          <div class="go">
+            <i class="fab fa-google"></i> Google
+          </div>
+          <div class="fb">
+            <i class="fab fa-facebook"></i> Facebook
+          </div>
+        </div>
+      </form>
+    </>
+  );
+}
