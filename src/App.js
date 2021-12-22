d7bb75f43a844e071585ac3f8149daf6034ba5fd
neilchetna <neelchetna18@gmail.com>
2021-12-23 01:34:59 +0530

    master commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import LandingPage from "./Componetns/LandingPage/LandingPage";
-import ParticlaAni from "./Componetns/ParticlaAni";
-import Login from "./Componetns/Signup/Login";
-import Signup from "./Componetns/Signup/Signup";
-import About from "./Componetns/About/About";
-import Dashboard from "./Componetns/Home/Dashboard";
+import Layout from "./Components/Layout";
+import Metrics from "./Components/Metrics/Metrics";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ParticlaAni />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/home" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Layout>
+      <Metrics />
+    </Layout>
   );
 }
 
